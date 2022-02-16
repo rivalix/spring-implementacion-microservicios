406002c61bc15cb0678f7112498a257fa1cbd469
Raul Alvarez <raul.alva1926@gmail.com>
2022-02-15 19:34:51 -0600

    Implementacion de Api Gateway

---
+++ api-gateway/src/test/java/com/wannacode/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wannacode.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
